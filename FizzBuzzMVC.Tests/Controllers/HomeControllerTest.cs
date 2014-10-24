805c0c6c720161085cd36b198a479200bf861b47
rjha999 <rjha999@gmail.com>
2014-10-24 16:31:42 +0530

    Demo FizzBuzz Project
    Demo Project to be pushed

---
+++ FizzBuzzMVC.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzMVC;
+using FizzBuzzMVC.Controllers;
+
+namespace FizzBuzzMVC.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Given10Return()
+        {
+            FizzBuzzController fizz = new FizzBuzzController();
+            ViewResult result = fizz.Index(10) as ViewResult;
+            string expected="1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz ";
+            string actual = result.ViewBag.OutPut;
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+    }
+}
---
+++ FizzBuzzMVC/Controllers/FizzBuzzController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FizzBuzzMVC.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        //
+        // GET: /FizzBuzz/
+        public ActionResult Index(int data)
+        {
+            for (int i = 1; i<= data; i++)
+            {
+
+
+                if (i % 3 == 0)
+                {
+                    ViewBag.OutPut += "Fizz ";
+                }
+                else if (i % 5 == 0)
+                {
+                    ViewBag.OutPut += "Buzz ";
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    ViewBag.Output += "FizzBuzz ";
+                }
+                else
+                {
+                    ViewBag.Output +=i.ToString() + " ";
+                }
+            }
+
+            List<string> first = new List<string> { "A", "B" };
+
+            Dictionary<int,string> second = new Dictionary<int,string> { {1,"A"}, {2,"B"} };
+
+            var temp= from f in first
+                      join s in second on f equals second.Keys
+
+            return View();
+        }
+	}
+}
